50084da9d0598995770c08caaf206cad321e9b54
Mahesh <maheshdasoffice9@gmail.com>
2023-10-04 11:49:29 +0530

    Initial commit

---
+++ src/test/java/com/ServiceRegistry/Code3M/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServiceRegistry.Code3M.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
